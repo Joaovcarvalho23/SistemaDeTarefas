7e672a21a6a711b6a9af689f6b610d15529660fb
Joao <joaovictorcordeiro00@gmail.com>
2023-09-18 09:50:12 -0300

    Criando o nosso DbContext

---
+++ Data/SistemaDeTarefasDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaDeTarefasDBContext : DbContext
+    {
+        public SistemaDeTarefasDBContext(DbContextOptions<SistemaDeTarefasDBContext> options)
+            : base(options)
+        {}
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+//por estarmos usando um entity framework, aqui estamos trabalhando com um ORM, ele vai facilitar a gente a trabalhar com banco de dados e independente do banco de dados também.
+//O ORM ele vai fazer toda a estrutura de entidade pra dentro do código, e depois vamos criar o nosso banco de dados e as tabelas através do nosso código Csharp.
+//Nesse DbContext, estaremos configurando nossas tabelas e as configurações do banco de dados. por exemplo, o nosso UsuarioModel representa uma tabela no banco de dados, a tabela usuários. 
