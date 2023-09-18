4e723a2f84552c07b72efe57048468e10440e87b
Joao <joaovictorcordeiro00@gmail.com>
2023-09-18 10:06:06 -0300

    Implementando a interface do repositorio de usuarios com seus respectivos contratos de usuário

---
+++ Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    //Aqui vão ter nossos contratos de usuário (buscar, add, revomer,...)
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();//task = assincrona
+        Task<UsuarioModel> BuscarPorId(int id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
+    }
+}
