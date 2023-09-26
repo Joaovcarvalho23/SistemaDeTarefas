f6153e3f2fd76bbb169e9dbfa25a9726214747f0
Joao <joaovictorcordeiro00@gmail.com>
2023-09-26 10:42:32 -0300

    modificando TarefaModel para atribuir uma tarefa a um usuário

--- Models/TarefaModel.cs
+++ Models/TarefaModel.cs
@@ -8,5 +8,9 @@ namespace SistemaDeTarefas.Models
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
         public StatusTarefa Status { get; set; }
+        public int? UsuarioId { get; set; }
+
+        //como iremos ter uma UsuarioId, iremos fazer uma referência. Iremos ter uma propriedade do tipo UsuarioModel
+        public virtual UsuarioModel? Usuario { get; set; }//se vamos ter uma referência/ligação com usuário com a tarefa, e demos o atributo de UsuarioId, ele vai procurar a referência da classe agregada exatamente com o mesmo nome do nosso atributo, removendo o Id do nome 'UsuarioId'. O prórpio EntityFramework já percebe isso.
     }
 }
