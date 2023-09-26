ebb80879c1928519d5e4e02266f7847b369c3325
Joao <joaovictorcordeiro00@gmail.com>
2023-09-26 09:54:40 -0300

    Implementando as APIs do UsuarioController

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
 
 namespace SistemaDeTarefas.Controllers
 {
@@ -8,10 +9,36 @@ namespace SistemaDeTarefas.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()//nome do nosso endpoint
+        private readonly IUsuarioRepositorio _usuarioRepositorio; //atributo
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio) //construtor
         {
-            return Ok();
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios()//nome do nosso endpoint
+        {
+           List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
+            return Ok(usuarios);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
+            return Ok(usuario);
+        }
+
+
+        [HttpPost]//vamos cadastrar
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)//nós vamos receber via body, ou seja, pelo corpo dessa requisição, uma UsuarioModel
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+            return Ok(usuario);
         }
     }
 }
